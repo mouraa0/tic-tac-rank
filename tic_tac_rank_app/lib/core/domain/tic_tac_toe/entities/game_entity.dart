e6ff793fd30ff029678bf55cfdcc6dc15b398f79
mouraa0 <jpcmouraa@gmail.com>
2023-04-02 19:03:46 -0300

    feat: implement methods in game entity

--- tic_tac_rank_app/lib/core/domain/tic_tac_toe/entities/game_entity.dart
+++ tic_tac_rank_app/lib/core/domain/tic_tac_toe/entities/game_entity.dart
@@ -3,11 +3,63 @@ import 'package:tic_tac_rank_app/core/domain/tic_tac_toe/entities/player_entity.
 class GameEntity {
   final PlayerEntity player1;
   final PlayerEntity player2;
-  final List<List<int>> gameTable;
+  late PlayerEntity currentPlayer;
 
   GameEntity({
     required this.player1,
     required this.player2,
-    required this.gameTable,
-  });
+  }) {
+    currentPlayer = player1;
+  }
+
+  List<List<String>> board = List.generate(3, (_) => List.filled(3, ''));
+
+  void changeCurrentPlayer() {
+    if (currentPlayer.id == player1.id) {
+      currentPlayer = player2;
+      return;
+    }
+
+    currentPlayer = player1;
+  }
+
+  void makeMove({
+    required int x,
+    required int y,
+  }) {
+    board[x][y] = currentPlayer.symbol;
+    changeCurrentPlayer();
+  }
+
+  void resetBoard() {
+    board = List.generate(3, (_) => List.filled(3, ''));
+    currentPlayer = player1;
+  }
+
+  bool isGameOver() {
+    for (int i = 0; i < 3; i++) {
+      if (board[i][0] == board[i][1] &&
+          board[i][1] == board[i][2] &&
+          board[i][0].isNotEmpty) {
+        return true;
+      }
+      if (board[0][i] == board[1][i] &&
+          board[1][i] == board[2][i] &&
+          board[0][i].isNotEmpty) {
+        return true;
+      }
+    }
+    if (board[0][0] == board[1][1] &&
+        board[1][1] == board[2][2] &&
+        board[0][0].isNotEmpty) {
+      return true;
+    }
+    if (board[0][2] == board[1][1] &&
+        board[1][1] == board[2][0] &&
+        board[0][2].isNotEmpty) {
+      return true;
+    }
+
+    return !board.any((row) => row.any((cell) => cell.isEmpty));
+  }
 }
--- tic_tac_rank_app/lib/core/domain/tic_tac_toe/entities/player_entity.dart
+++ tic_tac_rank_app/lib/core/domain/tic_tac_toe/entities/player_entity.dart
@@ -2,10 +2,12 @@ class PlayerEntity {
   final String id;
   final String name;
   final String image;
+  final String symbol;
 
   PlayerEntity({
     required this.id,
     required this.name,
     required this.image,
+    required this.symbol,
   });
 }
