424adedff737d0ef6512291b7bd00988998578b8
mouraa0 <jpcmouraa@gmail.com>
2023-04-13 11:21:59 -0300

    feat: handle matchmaking channel creation in controller in app

--- tic_tac_rank_app/lib/presenter/matchmaking/controller/matchmaking_controller.dart
+++ tic_tac_rank_app/lib/presenter/matchmaking/controller/matchmaking_controller.dart
@@ -1,12 +1,17 @@
 import 'dart:convert';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:tic_tac_rank_app/data/matchmaking/models/snapshot_model.dart';
-import 'package:tic_tac_rank_app/domain/matchmaking/entities/snapshot_entity.dart';
+import 'package:tic_tac_rank_app/core/routes/app_router.dart';
+import 'package:tic_tac_rank_app/data/matchmaking/models/matchmaking_message_model.dart';
+import 'package:tic_tac_rank_app/domain/matchmaking/entities/matchmaking_message_entity.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
 
 class MatchmakingController extends GetxController {
-  MatchmakingSnapshotEntity? mostRecentData;
+  final channel = Rxn<WebSocketChannel>();
+
+  MatchmakingMessageEntity? mostRecentData;
 
   void handleSnapshotReceive(AsyncSnapshot<dynamic> snapshot) {
     if (snapshot.data == null) {
@@ -14,6 +19,29 @@ class MatchmakingController extends GetxController {
     }
 
     mostRecentData =
-        MatchmakingSnapshotModel.fromJson(jsonDecode(snapshot.data));
+        MatchmakingMessageModel.fromJson(jsonDecode(snapshot.data));
+
+    if (mostRecentData!.msg == 'Found match') {
+      return _goToMatchScreen();
+    }
+  }
+
+  void _goToMatchScreen() {
+    Get.toNamed('${AppRouter.matchScreen}/${mostRecentData!.roomId}');
+  }
+
+  @override
+  void onInit() {
+    super.onInit();
+    const String url = !kIsWeb
+        ? 'ws://10.0.2.2:8000/matchmaking/122'
+        : 'ws://localhost:8000/matchmaking/121';
+    channel.value = WebSocketChannel.connect(Uri.parse(url));
+  }
+
+  @override
+  void dispose() {
+    channel.value?.sink.close();
+    super.dispose();
   }
 }
--- tic_tac_rank_app/lib/presenter/matchmaking/view/matchmaking_screen.dart
+++ tic_tac_rank_app/lib/presenter/matchmaking/view/matchmaking_screen.dart
@@ -1,46 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:tic_tac_rank_app/presenter/matchmaking/controller/matchmaking_controller.dart';
-import 'package:web_socket_channel/web_socket_channel.dart';
 
-class MatchmakingScreen extends StatefulWidget {
+class MatchmakingScreen extends StatelessWidget {
   const MatchmakingScreen({super.key});
 
   @override
-  State<MatchmakingScreen> createState() => _MatchmakingScreenState();
-}
-
-class _MatchmakingScreenState extends State<MatchmakingScreen> {
-  final _channel = WebSocketChannel.connect(
-    Uri.parse('ws://10.0.2.2:8000/matchmaking/121'),
-  );
-
-  @override
-  void dispose() {
-    _channel.sink.close();
-    super.dispose();
-  }
-
-  final _controller = MatchmakingController();
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: StreamBuilder(
-        stream: _channel.stream,
-        builder: (context, snapshot) {
-          _controller.handleSnapshotReceive(snapshot);
+    final controller = Get.find<MatchmakingController>();
 
-          return Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const CircularProgressIndicator(color: Colors.black),
-                const SizedBox(height: 20),
-                Text(_controller.mostRecentData?.msg ?? ''),
-              ],
-            ),
-          );
-        },
+    return Scaffold(
+      body: Obx(
+        () => StreamBuilder(
+          stream: controller.channel.value?.stream,
+          builder: (context, snapshot) {
+            controller.handleSnapshotReceive(snapshot);
+
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const CircularProgressIndicator(color: Colors.black),
+                  const SizedBox(height: 20),
+                  Text(controller.mostRecentData?.msg ?? ''),
+                ],
+              ),
+            );
+          },
+        ),
       ),
     );
   }
