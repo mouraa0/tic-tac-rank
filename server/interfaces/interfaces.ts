5db9fd1544587711b9b5116b382dc4c338518993
mouraa0 <jpcmouraa@gmail.com>
2023-04-13 11:18:44 -0300

    feat: changes to interfaces in server

--- server/interfaces/interfaces.ts
+++ server/interfaces/interfaces.ts
@@ -7,15 +7,35 @@ export interface WaitingRoom {
 export interface GameRoom {
   player1Id: string;
   player2Id: string;
-  player1Socket: WebSocket;
-  player2Socket: WebSocket;
+  socketsArr: WebSocket[];
+  isPlayer1Connected: boolean;
+  isPlayer2Connected: boolean;
+  player1Socket: WebSocket | null;
+  player2Socket: WebSocket | null;
   roomId: string;
   board: string[][];
+  playerTurn: string;
+}
+
+export interface GameRoomMap {
+  [key: string]: GameRoom;
 }
 
 export interface MatchmakingHandlerParams {
   socket: WebSocket;
   waitingRoomsArr: WaitingRoom[];
-  gameRoomsArr: GameRoom[];
+  gameRoomsMap: GameRoomMap;
   userId: string;
+}
+
+export interface GameRoomConnectionHandlerParams {
+  socket: WebSocket;
+  userId: string;
+  roomData: GameRoom;
+}
+
+export interface GameRoomMessageReceived {
+  msg: string;
+  board: string[][];
+  status: number;
 }
