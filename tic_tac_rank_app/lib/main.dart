12e7a3b9e77c6e538a6ce215aacd68093a5c623e
mouraa0 <jpcmouraa@gmail.com>
2023-04-14 16:41:57 -0300

    feat: supabase setup in app

--- tic_tac_rank_app/lib/main.dart
+++ tic_tac_rank_app/lib/main.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
-// import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:tic_tac_rank_app/core/routes/app_router.dart';
 
 void main() async {
   await dotenv.load(fileName: ".env");
 
-  // await Supabase.initialize(
-  //   url: 'YOUR_SUPABASE_URL',
-  //   anonKey: 'YOUR_SUPABASE_ANON_KEY',
-  // );
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+  );
 
   runApp(const MyApp());
 }
