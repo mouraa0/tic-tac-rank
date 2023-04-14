2e5356a0d72da1f5b71090fda3b0f16c21b97741
mouraa0 <jpcmouraa@gmail.com>
2023-04-14 19:59:50 -0300

    feat: change register forms logic to use FormsUtils in app

--- tic_tac_rank_app/lib/presenter/register/controller/register_controller.dart
+++ tic_tac_rank_app/lib/presenter/register/controller/register_controller.dart
@@ -1,8 +1,7 @@
-import 'package:email_validator/email_validator.dart';
 import 'package:get/get.dart';
 import 'package:tic_tac_rank_app/core/routes/app_router.dart';
-import 'package:tic_tac_rank_app/core/supabase/supabase_constants.dart';
 import 'package:tic_tac_rank_app/core/supabase/utils/supabase_account_utils.dart';
+import 'package:tic_tac_rank_app/core/utils/forms/forms_utils.dart';
 
 class RegisterController extends GetxController {
   String email = '';
@@ -24,6 +23,7 @@ class RegisterController extends GetxController {
   }
 
   void _verifyRegisterButtonActive() {
+    //TODO: improve shitty logic;
     if (email.isNotEmpty &&
         emailErrorText.value == null &&
         password.isNotEmpty &&
@@ -39,54 +39,14 @@ class RegisterController extends GetxController {
   void onChangedEmail(String str) {
     email = str;
 
-    _validateEmail();
+    FormsUtils.validateEmail(email: email, errorText: emailErrorText);
     _verifyRegisterButtonActive();
   }
-
-  void _validateEmail() {
-    if (!EmailValidator.validate(email)) {
-      emailErrorText.value = 'not a valid email';
-
-      return;
-    }
-
-    emailErrorText.value = null;
-  }
-
-  // void onChangedUsername(String str) {
-  //   username = str;
-
-  //   _validateUsername();
-  //   _verifyRegisterButtonActive();
-  // }
-
-  // void _validateUsername() {
-  //   if (username.length > 14) {
-  //     usernameErrorText.value = 'username must be 14 characters or less.';
-
-  //     return;
-  //   }
-
-  //   usernameErrorText.value = null;
-  // }
 
   void onChangedPassword(String str) {
     password = str;
 
-    _validatePassword();
+    FormsUtils.validatePassword(psw: password, errorText: passwordErrorText);
     _verifyRegisterButtonActive();
-  }
-
-  void _validatePassword() {
-    RegExp passwordRegex =
-        RegExp(r'^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d!@#$%^&*]{8,}$');
-    if (!passwordRegex.hasMatch(password)) {
-      passwordErrorText.value =
-          'must be at least 8 characters with a letter and a number.';
-
-      return;
-    }
-
-    passwordErrorText.value = null;
   }
 }
