8af31e89d3a54c1e321eb236d7473a554e124d27
mouraa0 <jpcmouraa@gmail.com>
2023-04-14 15:40:14 -0300

    feat: .env setup

--- tic_tac_rank_app/lib/main.dart
+++ tic_tac_rank_app/lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
+// import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:tic_tac_rank_app/core/routes/app_router.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: ".env");
+
+  // await Supabase.initialize(
+  //   url: 'YOUR_SUPABASE_URL',
+  //   anonKey: 'YOUR_SUPABASE_ANON_KEY',
+  // );
+
   runApp(const MyApp());
 }
 
