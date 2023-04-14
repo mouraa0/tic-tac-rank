736c4e4a0a7cd900d31e556bbc4a7221c7392805
mouraa0 <jpcmouraa@gmail.com>
2023-04-13 23:38:07 -0300

    feat: add login screen to router in app

--- tic_tac_rank_app/lib/core/routes/app_router.dart
+++ tic_tac_rank_app/lib/core/routes/app_router.dart
@@ -1,7 +1,9 @@
+import 'package:get/get.dart';
 import 'package:get/get_navigation/src/routes/get_route.dart';
 import 'package:tic_tac_rank_app/presenter/game_room/binder/game_room_bindings.dart';
 import 'package:tic_tac_rank_app/presenter/game_room/view/game_room_screen.dart';
 import 'package:tic_tac_rank_app/presenter/home/view/home_screen.dart';
+import 'package:tic_tac_rank_app/presenter/login/view/login_screen.dart';
 import 'package:tic_tac_rank_app/presenter/matchmaking/binder/matchmaking_bindings.dart';
 import 'package:tic_tac_rank_app/presenter/matchmaking/view/matchmaking_screen.dart';
 
@@ -10,9 +12,15 @@ class AppRouter {
   static String matchmakingScreen = '/matchmaking';
   static String matchScreen = '/match';
   static String matchConfigRoute = '$matchScreen/:roomId';
+  static String loginScreen = '/login';
+  static String registerScreen = '/register';
 
   static returnPages() {
     return [
+      GetPage(
+        name: loginScreen,
+        page: () => const LoginScreen(),
+      ),
       GetPage(
         name: homeScreen,
         page: () => const HomeScreen(),
