b5052068142f7562bab99fffefb342e6623392f0
mouraa0 <jpcmouraa@gmail.com>
2023-04-15 03:33:01 -0300

    feat: implement login with another account text button in CreateUsernameScreen in app

--- tic_tac_rank_app/lib/presenter/register/controller/create_username_controller.dart
+++ tic_tac_rank_app/lib/presenter/register/controller/create_username_controller.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:tic_tac_rank_app/core/global/global.dart';
 import 'package:tic_tac_rank_app/core/routes/app_router.dart';
+import 'package:tic_tac_rank_app/core/supabase/supabase_constants.dart';
 import 'package:tic_tac_rank_app/core/supabase/utils/database_utils/supabase_database_utils.dart';
 import 'package:tic_tac_rank_app/core/utils/forms/forms_utils.dart';
 import 'package:tic_tac_rank_app/core/error/show_error_snack_bar/show_error_snack_bar_extension.dart';
@@ -27,6 +28,12 @@ class CreateUsernameController extends GetxController {
 
     isButtonLoading.value = false;
     snackbarKey.currentState?.showErrorSnackBar(exception: response.exception);
+  }
+
+  void loginWithAnotherAccount() {
+    supabase.auth.signOut();
+
+    Get.offAndToNamed(AppRouter.loginScreen);
   }
 
   void _verifyButtonActive() {
