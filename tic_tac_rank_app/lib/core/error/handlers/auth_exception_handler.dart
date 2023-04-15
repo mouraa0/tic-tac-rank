a39e844c00a2c61d44338350b7f99b4c824800ab
mouraa0 <jpcmouraa@gmail.com>
2023-04-15 02:13:10 -0300

    feat: added case to AuthExceptionHandler for "User already registered" situation in app

--- tic_tac_rank_app/lib/core/error/handlers/auth_exception_handler.dart
+++ tic_tac_rank_app/lib/core/error/handlers/auth_exception_handler.dart
@@ -1,8 +1,20 @@
+import 'package:get/get.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:tic_tac_rank_app/core/error/classes/error_info.dart';
+import 'package:tic_tac_rank_app/core/routes/app_router.dart';
 
 class AuthExceptionHandler {
   static ErrorInfo call(AuthException exception) {
-    return ErrorInfo(msg: exception.message);
+    switch (exception.message.toLowerCase()) {
+      case 'user already registered':
+        return ErrorInfo(
+          msg: exception.message,
+          buttonTitle: 'login',
+          onButtonPressed: () => Get.offAndToNamed(AppRouter.loginScreen),
+        );
+
+      default:
+        return ErrorInfo(msg: exception.message);
+    }
   }
 }
