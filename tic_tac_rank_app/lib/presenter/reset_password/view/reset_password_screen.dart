546d08a3fba671b705f56be5a244b536d4c4a3a8
mouraa0 <jpcmouraa@gmail.com>
2023-07-19 23:05:31 -0300

    ios setup

--- tic_tac_rank_app/lib/presenter/reset_password/view/reset_password_screen.dart
+++ tic_tac_rank_app/lib/presenter/reset_password/view/reset_password_screen.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:tic_tac_rank_app/core/routes/app_router.dart';
 import 'package:tic_tac_rank_app/core/widgets/buttons/app_button_big_widget.dart';
-import 'package:tic_tac_rank_app/core/widgets/forms/divider/forms_divider_widget.dart';
-import 'package:tic_tac_rank_app/core/widgets/forms/external_accounts_options/forms_external_accounts_options_widget.dart';
 import 'package:tic_tac_rank_app/core/widgets/forms/text_span/forms_text_span_widget.dart';
 import 'package:tic_tac_rank_app/core/widgets/forms/title/forms_title_widget.dart';
 import 'package:tic_tac_rank_app/core/widgets/textfield/text_field_widget.dart';
