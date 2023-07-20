df7909316de6f4a3c8db354d5ba25b72e322a058
mouraa0 <jpcmouraa@gmail.com>
2023-07-20 00:16:25 -0300

    feat: add logo widget to forms title

---
+++ tic_tac_rank_app/lib/core/widgets/title/forms_title_widget.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:tic_tac_rank_app/core/styles/text/text_styles.dart';
+import 'package:tic_tac_rank_app/core/widgets/logo/logo.dart';
+
+class AppFormsTitleWidget extends StatelessWidget {
+  final List<String> subtitle;
+  final bool showLogo;
+
+  const AppFormsTitleWidget({
+    Key? key,
+    required this.subtitle,
+    this.showLogo = true,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        if (showLogo) ...[
+          const AppLogo(),
+          const SizedBox(height: 10),
+        ],
+        RichText(
+          text: TextSpan(
+            children: [
+              TextSpan(
+                text: subtitle[0],
+                style: AppTextStyles.pageSubtitleHighlighted,
+              ),
+              TextSpan(
+                text: subtitle[1],
+                style: AppTextStyles.pageSubtitle,
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
