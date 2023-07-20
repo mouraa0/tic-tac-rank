ad2f308c661ec77095c30e26a8e17bc610c1e172
mouraa0 <jpcmouraa@gmail.com>
2023-07-20 00:15:44 -0300

    feat: add isExpanded property to app button

--- tic_tac_rank_app/lib/core/widgets/buttons/app_button_widget.dart
+++ tic_tac_rank_app/lib/core/widgets/buttons/app_button_widget.dart
@@ -3,25 +3,30 @@ import 'package:flutter/material.dart';
 class AppButton extends StatelessWidget {
   final String title;
   final Function()? onPressed;
+  final bool isExpanded;
 
   const AppButton({
     Key? key,
     required this.title,
     required this.onPressed,
+    this.isExpanded = true,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: onPressed,
-      style: ElevatedButton.styleFrom(
-        elevation: 0,
-        backgroundColor: Colors.white,
-        side: const BorderSide(color: Colors.black),
-      ),
-      child: Text(
-        title,
-        style: const TextStyle(color: Colors.black),
+    return SizedBox(
+      width: isExpanded ? double.infinity : null,
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          elevation: 0,
+          backgroundColor: Colors.white,
+          side: const BorderSide(color: Colors.black),
+        ),
+        child: Text(
+          title,
+          style: const TextStyle(color: Colors.black),
+        ),
       ),
     );
   }
